581790a3f49f96cafdbae5e1cfbcc35bee09c8b1
Uilton <uilton_3001@outlook.com>
2023-10-24 15:34:41 -0300

    Feat: subindo projeto java

---
+++ RestFulMaturidade/src/main/java/br/com/RestSpringMaturidade/RestFulMaturidade/RestFulMaturidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.RestSpringMaturidade.RestFulMaturidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulMaturidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulMaturidadeApplication.class, args);
+	}
+
+}
---
+++ RestFulMaturidade/src/test/java/br/com/RestSpringMaturidade/RestFulMaturidade/RestFulMaturidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.RestSpringMaturidade.RestFulMaturidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulMaturidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
